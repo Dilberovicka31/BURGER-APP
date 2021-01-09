5bdcde02b7156297840b20d8cf7a1370a78e0b09
Dilberovicka31 <dilberovicm36@gmail.com>
2021-01-09 09:58:00 -0800

    added route for update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,10 +5,39 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 router.get("/", function (req, res) {
-    burger.selectAll(function (data) {
-      console.log(data);
-      res.render("index", {
-        burgers: data,
-      });
+  burger.selectAll(function (data) {
+    console.log(data);
+    res.render("index", {
+      burgers: data,
     });
   });
+});
+
+router.post("/api/burgers", function (req, res) {
+  burger.insertBurger(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+    function (result) {
+      //Send back the id of the new burger
+      res.json({ id: result.insertId });
+    }
+  );
+});
+router.put("/api/burgers/:id", function (req, res) {
+  const condition = "id = " + req.params.id;
+  console.log("condition", condition);
+  burger.updateBurger(
+    { devoured: req.body.devoured },
+    condition,
+    function (result) {
+      if (result.changedRows === 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
+});
+
+module.exports = router;
+//Controller routes calls the model
